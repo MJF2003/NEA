fdc2a999999e224655f1fc4db478404f192063a2
Michael Fahey <michael.fahey@challoners.org>
2022-03-17 14:04:21 +0000

    restore func.py

--- UI/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5 import QtWidgets, QtCore
-
-app = QtWidgets.QApplication(sys.argv)
-widget = QtWidgets.QWidget()
-widget.resize(400, 200)
-widget.setWindowTitle("This is PyQt Widget example")
-widget.show()
-exit(app.exec_()) 

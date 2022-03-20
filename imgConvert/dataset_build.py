f3fe1195a77a6fe7bd18a2288cc976204f1d9b2a
Michael Fahey <michael.fahey@challoners.org>
2022-03-20 22:03:20 +0000

    all sorts of data nonsense

--- imgConvert/dataset_build.py
+++ imgConvert/dataset_build.py
@@ -2,15 +2,15 @@ import PIL.Image
 import os
 from pathlib import Path
 
-from main import full_edges
 
-dir_path = Path("../../dataset")
+
+dir_path = Path("../dataset")
 
 
 for idx, filename in enumerate(os.listdir(dir_path)):
     im = PIL.Image.open(dir_path / filename)
     im = im.resize((350, 350))
-    loc = f"{dir_path / 'bmp' / str(idx)}.bmp"
+    loc = f"../data/raw/{idx}.bmp"
     im.save(loc)
 
 

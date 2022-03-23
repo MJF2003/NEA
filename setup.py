ca55685c11eda863f38d209e825e8f2dbd3ed3b7
Michael Fahey <michael.fahey@challoners.org>
2022-03-23 18:43:44 +0000

    packaging 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='NEA',
     version='1.0dev',
     packages=['src', 'src.module0', 'src.module1', 'src.module2'],
-    url='',
+    url='https://github.com/MJF2003/NEA',
     license='',
     author='Michael Fahey',
     author_email='michaelfahey42@gmail.com',

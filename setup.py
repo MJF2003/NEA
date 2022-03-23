7403c5834cd2a7c25c810f00eee2fbc782d8bd1c
Michael Fahey <michael.fahey@challoners.org>
2022-03-23 18:31:44 +0000

    begin project packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NEA',
+    version='1.0dev',
+    packages=['src', 'src.module0', 'src.module1', 'src.module2'],
+    url='',
+    license='',
+    author='Michael Fahey',
+    author_email='michaelfahey42@gmail.com',
+    description='Road Sign ID NEA'
+)

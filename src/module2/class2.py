a41f0f3d8349d7f6299908048f7a7bf7772f964d
Michael Fahey <michael.fahey@challoners.org>
2022-03-30 11:36:05 +0100

    current state of affairs

--- src/module2/class2.py
+++ src/module2/class2.py
@@ -12,63 +12,67 @@ class_names = ['natspdlim', 'rdnarrows', 'thirtymph']
 
 
 def build_model(dataset_loc: Path):  # A very procedural function to assemble an ML model
+    try:
+        # Extract Datasets from directory
+        train_ds = tf.keras.utils.image_dataset_from_directory(  # Define the training set from the directory
+            dataset_loc,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size
+        )
+        val_ds = tf.keras.utils.image_dataset_from_directory(  # Same but for validation
+            dataset_loc,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size
+        )
 
-    # Extract Datasets from directory
-    train_ds = tf.keras.utils.image_dataset_from_directory(  # Define the training set from the directory
-        dataset_loc,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size
-    )
-    val_ds = tf.keras.utils.image_dataset_from_directory(  # Same but for validation
-        dataset_loc,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size
-    )
 
-    my_classes = train_ds.class_names
-    num_classes = len(my_classes)  # Number of possible classes the images could fall into
+        my_classes = train_ds.class_names
+        num_classes = len(my_classes)  # Number of possible classes the images could fall into
 
-    resize_rescale = tf.keras.Sequential([  # Resizing the dataset components to square and correct size
-        tf.keras.layers.Resizing(img_height, img_width),
-        tf.keras.layers.Rescaling(1./255)  # Pixel vals to between 0 and 1
-    ])
-    data_augmentation = tf.keras.Sequential([  # Augmentation creates additional examples from the defined dataset
-        tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-        tf.keras.layers.RandomRotation(0.2),
-    ])
+        resize_rescale = tf.keras.Sequential([  # Resizing the dataset components to square and correct size
+            tf.keras.layers.Resizing(img_height, img_width),
+            tf.keras.layers.Rescaling(1./255)  # Pixel vals to between 0 and 1
+        ])
+        data_augmentation = tf.keras.Sequential([  # Augmentation creates additional examples from the defined dataset
+            tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+            tf.keras.layers.RandomRotation(0.2),
+        ])
 
-    # Start of Optimisation Features as described by tensorflow docs
-    AUTOTUNE = tf.data.AUTOTUNE
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-    # End of Optimisation Fetaures
+        # Start of Optimisation Features as described by tensorflow docs
+        AUTOTUNE = tf.data.AUTOTUNE
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        # End of Optimisation Fetaures
 
-    # Start of main model
-    model = tf.keras.Sequential([
-        resize_rescale,
-        data_augmentation,
+        # Start of main model
+        model = tf.keras.Sequential([
+            resize_rescale,
+            data_augmentation,
 
-        tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(num_classes)
-    ])
-    # End of main model
+            tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(32, activation='relu'),
+            tf.keras.layers.Dense(num_classes)
+        ])
+        # End of main model
 
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    return model, train_ds, val_ds
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+        return model, train_ds, val_ds
+    except:
+        print("Missing project dependencies. Ensure all are present")
 
 
 def train(model, train_ds, val_ds, save_loc, epochs):
